4615398b8f2f5c2bf0ee360ddcceb6aff94f1d4d
mehmed <mehmedm325@gmail.com>
2023-11-07 21:48:40 +0200

    init

---
+++ src/main/java/bg/softuni/homefurniture/HomefurnitureApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.homefurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefurnitureApplication.class, args);
+	}
+
+}
