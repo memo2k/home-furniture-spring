013feb2c5edfb92d1382a6e8ca3799df82885ac7
mehmed <mehmedm325@gmail.com>
2023-11-30 00:12:45 +0200

    create auth functionality

--- src/main/java/bg/softuni/homefurniture/controller/UserController.java
+++ src/main/java/bg/softuni/homefurniture/controller/UserController.java
@@ -1,18 +1,56 @@
 package bg.softuni.homefurniture.controller;
 
+import bg.softuni.homefurniture.model.dto.UserLoginBindingModel;
+import bg.softuni.homefurniture.model.dto.UserRegisterBindingModel;
+import bg.softuni.homefurniture.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public ModelAndView login() {
-        return new ModelAndView("login.html");
+        return new ModelAndView("login");
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(UserLoginBindingModel userLoginBindingModel) {
+        boolean isLogged = userService.login(userLoginBindingModel);
+
+        if (isLogged) {
+            return new ModelAndView("redirect:/");
+        }
+
+        return new ModelAndView("login");
+
     }
 
     @GetMapping("/register")
     public ModelAndView register() {
-        return new ModelAndView("register.html");
+        return new ModelAndView("register");
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(UserRegisterBindingModel userRegisterBindingModel) {
+        this.userService.register(userRegisterBindingModel);
+
+        return new ModelAndView("redirect:login");
+    }
+
+    @PostMapping("/logout")
+    public ModelAndView logout() {
+        this.userService.logout();
+
+        return new ModelAndView("redirect:/");
     }
 }
--- src/main/java/bg/softuni/homefurniture/service/UserService.java
+++ src/main/java/bg/softuni/homefurniture/service/UserService.java
@@ -1,9 +1,15 @@
 package bg.softuni.homefurniture.service;
 
+import bg.softuni.homefurniture.model.dto.UserLoginBindingModel;
+import bg.softuni.homefurniture.model.dto.UserRegisterBindingModel;
 import bg.softuni.homefurniture.model.entity.User;
 
 import java.util.Set;
 
 public interface UserService {
-    Set<User> getUser();
+    void register(UserRegisterBindingModel userRegisterBindingModel);
+
+    boolean login(UserLoginBindingModel userLoginBindingModel);
+
+    void logout();
 }
