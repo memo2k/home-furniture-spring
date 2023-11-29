14da8dff1b56e4d235975358ae14803924feb9b2
mehmed <mehmedm325@gmail.com>
2023-11-30 00:13:45 +0200

    create logged user component

---
+++ src/main/java/bg/softuni/homefurniture/service/session/LoggedUser.java
@@ -0,0 +1,38 @@
+package bg.softuni.homefurniture.service.session;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class LoggedUser {
+    private String username;
+
+    private String email;
+
+    private boolean isLogged;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isLogged() {
+        return isLogged;
+    }
+
+    public void setLogged(boolean logged) {
+        isLogged = logged;
+    }
+}
