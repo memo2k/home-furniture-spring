9adac2f38a80d37d47624352fa9fe80ece0b3e63
mehmed <mehmedm325@gmail.com>
2023-12-01 00:06:50 +0200

    update Product Service

--- src/main/java/bg/softuni/homefurniture/service/ProductService.java
+++ src/main/java/bg/softuni/homefurniture/service/ProductService.java
@@ -1,9 +1,10 @@
 package bg.softuni.homefurniture.service;
 
+import bg.softuni.homefurniture.model.dto.AddProductBindingModel;
 import bg.softuni.homefurniture.model.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    List<Product> getAllProducts();
+    void addProduct(AddProductBindingModel addProductBindingModel);
 }
