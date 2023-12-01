d06d7de31ffe9e7d988a63c2df1c89eb118dc2a0
mehmed <mehmedm325@gmail.com>
2023-12-01 23:02:29 +0200

    add new product method

--- src/main/java/bg/softuni/homefurniture/service/ProductService.java
+++ src/main/java/bg/softuni/homefurniture/service/ProductService.java
@@ -2,9 +2,13 @@ package bg.softuni.homefurniture.service;
 
 import bg.softuni.homefurniture.model.dto.AddProductBindingModel;
 import bg.softuni.homefurniture.model.entity.Product;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
     void addProduct(AddProductBindingModel addProductBindingModel);
+
+    List<Product> getAll();
 }
--- src/main/java/bg/softuni/homefurniture/service/impl/ProductServiceImpl.java
+++ src/main/java/bg/softuni/homefurniture/service/impl/ProductServiceImpl.java
@@ -1,11 +1,18 @@
 package bg.softuni.homefurniture.service.impl;
 
 import bg.softuni.homefurniture.model.dto.AddProductBindingModel;
+import bg.softuni.homefurniture.model.entity.Category;
 import bg.softuni.homefurniture.model.entity.Product;
+import bg.softuni.homefurniture.repository.CategoryRepository;
 import bg.softuni.homefurniture.repository.ProductRepository;
 import bg.softuni.homefurniture.service.ProductService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -20,7 +27,13 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void addProduct(AddProductBindingModel addProductBindingModel) {
         Product product = modelMapper.map(addProductBindingModel, Product.class);
+        product.setCreatedOn(LocalDate.now());
 
         productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productRepository.findAll();
     }
 }
