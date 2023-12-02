9b46c0c2a92b5840858f88d6e1133e387135132c
mehmed <mehmedm325@gmail.com>
2023-12-02 23:58:00 +0200

    update product functionality

--- src/main/java/bg/softuni/homefurniture/controller/ProductController.java
+++ src/main/java/bg/softuni/homefurniture/controller/ProductController.java
@@ -1,39 +1,49 @@
 package bg.softuni.homefurniture.controller;
 
-import bg.softuni.homefurniture.model.dto.AddProductBindingModel;
+import bg.softuni.homefurniture.model.dto.binding.AddProductBindingModel;
+import bg.softuni.homefurniture.model.dto.view.ProductDetailsViewModel;
+import bg.softuni.homefurniture.model.entity.Comment;
 import bg.softuni.homefurniture.model.enums.CategoryName;
+import bg.softuni.homefurniture.service.CommentService;
 import bg.softuni.homefurniture.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
     private final ProductService productService;
+    private final CommentService commentService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, CommentService commentService) {
         this.productService = productService;
+        this.commentService = commentService;
     }
 
-    // will take an id
-    @GetMapping("/product")
-    public ModelAndView singleProduct() {
-        return new ModelAndView("product");
-    }
+    @GetMapping("/product/{id}")
+    public ModelAndView singleProduct(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("product");
 
-    @GetMapping("/all-products")
-    public ModelAndView allProducts() {
-        ModelAndView modelAndView = new ModelAndView("admin/products-list");
-        modelAndView.addObject("products", productService.getAll());
+        ProductDetailsViewModel product = productService.getDetails(id);
+        List<Comment> comments = commentService.orderCommentsByDateDesc(product.getComments());
+
+        modelAndView.addObject("product", product);
+        modelAndView.addObject("comments", comments);
 
         return modelAndView;
     }
 
-    @GetMapping("/favorite-products")
-    public ModelAndView favoriteProducts() {
-        return new ModelAndView("favorite-products");
+    @GetMapping("/all-products")
+    public ModelAndView allProducts() {
+        ModelAndView modelAndView = new ModelAndView("all-products");
+
+        modelAndView.addObject("products", productService.getAll());
+
+        return modelAndView;
     }
 
     @GetMapping("/admin/add-product")
