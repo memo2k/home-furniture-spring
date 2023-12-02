a077b7b97009559e776d0da53eb88081d6164825
mehmed <mehmedm325@gmail.com>
2023-12-02 23:55:38 +0200

    update authentication service

--- src/main/java/bg/softuni/homefurniture/service/AuthenticationService.java
+++ src/main/java/bg/softuni/homefurniture/service/AuthenticationService.java
@@ -1,15 +1,15 @@
 package bg.softuni.homefurniture.service;
 
-import bg.softuni.homefurniture.model.dto.UserLoginBindingModel;
-import bg.softuni.homefurniture.model.dto.UserRegisterBindingModel;
+import bg.softuni.homefurniture.model.dto.binding.UserLoginBindingModel;
+import bg.softuni.homefurniture.model.dto.binding.UserRegisterBindingModel;
 import bg.softuni.homefurniture.model.entity.User;
 
-import java.util.Set;
-
-public interface UserService {
+public interface AuthenticationService {
     void register(UserRegisterBindingModel userRegisterBindingModel);
 
     boolean login(UserLoginBindingModel userLoginBindingModel);
 
     void logout();
+
+    User getUserById(Long id);
 }
