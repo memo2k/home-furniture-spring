41106fda1fe5dda1620a16097cccc0649763c264
mehmed <mehmedm325@gmail.com>
2023-12-02 23:55:25 +0200

    update LoggedUser

--- src/main/java/bg/softuni/homefurniture/service/session/LoggedUser.java
+++ src/main/java/bg/softuni/homefurniture/service/session/LoggedUser.java
@@ -6,11 +6,21 @@ import org.springframework.web.context.annotation.SessionScope;
 @Component
 @SessionScope
 public class LoggedUser {
+    private Long id;
+
     private String username;
 
     private String email;
 
     private boolean isLogged;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
