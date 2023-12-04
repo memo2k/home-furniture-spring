fc3c8a9b2b8c42122c9f2e36c72a1ee302a10b3c
mehmed <mehmedm325@gmail.com>
2023-12-05 00:34:34 +0200

    create user roles init

---
+++ src/main/java/bg/softuni/homefurniture/init/UserRolesInit.java
@@ -0,0 +1,38 @@
+package bg.softuni.homefurniture.init;
+
+import bg.softuni.homefurniture.model.entity.Role;
+import bg.softuni.homefurniture.model.enums.UserRoles;
+import bg.softuni.homefurniture.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class UserRolesInit implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+
+    public UserRolesInit(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        boolean hasRoles = roleRepository.count() > 0;
+
+        if (!hasRoles) {
+            Set<Role> roles = new HashSet<>();
+
+            Arrays.stream(UserRoles.values())
+                    .forEach(userRole -> {
+                        Role role = new Role();
+                        role.setName(userRole);
+                        roles.add(role);
+                    });
+
+            roleRepository.saveAll(roles);
+        }
+    }
+}
