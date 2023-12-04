6c185bc912f292ccb79cedff4874e33f83d803b4
mehmed <mehmedm325@gmail.com>
2023-12-05 00:36:46 +0200

    update role repository

--- src/main/java/bg/softuni/homefurniture/repository/RoleRepository.java
+++ src/main/java/bg/softuni/homefurniture/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package bg.softuni.homefurniture.repository;
 
 import bg.softuni.homefurniture.model.entity.Role;
+import bg.softuni.homefurniture.model.enums.UserRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(UserRoles role);
 }
