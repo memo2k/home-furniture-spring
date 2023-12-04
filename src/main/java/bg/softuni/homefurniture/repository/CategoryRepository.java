27b7a8cf06160828f6383dc5442a23e83cf88929
mehmed <mehmedm325@gmail.com>
2023-12-05 00:36:22 +0200

    update category repository

--- src/main/java/bg/softuni/homefurniture/repository/CategoryRepository.java
+++ src/main/java/bg/softuni/homefurniture/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package bg.softuni.homefurniture.repository;
 
 import bg.softuni.homefurniture.model.entity.Category;
+import bg.softuni.homefurniture.model.enums.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Set<Category> getAllByNameIn(Set<CategoryName> categoryNames);
 }
